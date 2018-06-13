7cbbb6c1abd52fbd7e8250ed19b79bf8d1e3b954
bhg <40246871+begghg@users.noreply.github.com>
2018-06-13 22:11:59 +0200

    Create AppMain
    clase public void

---
+++ AppMain.java
@@ -0,0 +1,40 @@
+package operaciones;
+
+import java.util.Scanner;
+
+public class AppMain {
+	static Scanner sc = new Scanner(System.in);
+	public static double pedirNumero(){
+		double num;
+		System.out.print("Introduce numero: ");
+		num = sc.nextDouble();
+		return num;
+		
+	}
+
+	public static void main(String[] args) {
+			
+		Operaciones op = new Suma();
+				
+		//doy valor a los atributos x,y 
+		op.setX(pedirNumero());
+		op.setY( pedirNumero());
+		
+		//getX, getY recupero el valor del atributo
+		
+		Suma c= new Suma(op.getX(),op.getY());
+		 c.ver();
+		
+		Operaciones f= new Producto(op.getX(),op.getY());
+		f.ver();
+	
+		if(op.getY()!=0){
+			Operaciones f1= new Division(op.getX(),op.getY());
+			f1.ver();
+		}
+		else
+			System.out.println("No se puede dividir entre cero");
+
+	}
+
+}
